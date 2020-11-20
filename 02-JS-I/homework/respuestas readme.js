028ef7c33028c3e1f652a84eba92ad73daafa1da
GerardoPadvalskis <gerardo.padvalskis@gmail.com>
2020-11-20 16:16:06 -0300

    agregando ejercicios de la unidad 1

---
+++ 02-JS-I/homework/respuestas readme.js
@@ -0,0 +1,7 @@
+//las variables son cajones donde se guarda informacion que luego reutilizaremos.
+//los strings son cadenas de caracteres que se definen al poner estos caracteres entre comillas
+//las funciones son esencialmente para no repertir codigo que utilizaremos muchas veces.
+//los argumentos de una funcion van entre parentecis y son un tipo de varible.
+//al declarar return, nos retorna un resultado.
+//la declaracion if es un condicional si haces esto da esto y si haces esto otro da esto otro.
+//los valores booleanos solo nos dara uno de dos resultados, siempre seran solo dos opciones y cumplira con una sola de ellas la operacion dada.
